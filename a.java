7cf34f294fd16325110b08802cff0370dadd47ca
su0105 <63032005+su0105@users.noreply.github.com>
2020-04-19 20:32:15 +0800

    创建a.java

---
+++ a.java
@@ -0,0 +1,13 @@
+package com;
+
+public class a {
+    public static  void main(String[] args){
+        System.out.println("holle word");
+        //这是单行注释
+        String s1=new String();
+        s1.toString();
+
+        System.out.println();
+
+    }
+}
